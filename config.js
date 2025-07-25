5b22ccfe1f1eb2fb85075fc101433f4673c63d35
anugrahffxe <rroyalmass@gmail.com>
2025-07-25 15:31:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ if (fs.existsSync("config.env")) {
 	});
 }
 
-const DATABASE_URL = process.env.DATABASE_URL || "postgresql://neondb_owner:npg_yzqIj3CEXp2d@ep-young-fire-a2xx84dw-pooler.eu-central-1.aws.neon.tech/neondb?sslmode"; // Corrected SQLite format
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://dicruz_qhkp_user:sHtiy6SKCkTIXHkl2aCHgnyv3QicX7q8@dpg-d21l7bndiees73dvelig-a/dicruz_qhkp"; // Corrected SQLite format
 
 if (!DATABASE_URL.startsWith("sqlite://") && !DATABASE_URL.startsWith("postgres://") && !DATABASE_URL.startsWith("postgresql://")) {
 	throw new Error("Invalid DATABASE_URL format. Use 'sqlite://' or 'postgres://'");
@@ -41,13 +41,13 @@ DATABASE.authenticate().then(() => console.log("Database connection established 
 
 module.exports = {
 	VERSION: require("./package.json").version,
-	ALIVE: process.env.ALIVE || "Hello i'm alive",
+	ALIVE: process.env.ALIVE || "HEY ROYAL IS ALIVE🫶",
 	ALWAYS_ONLINE: toBool(process.env.ALWAYS_ONLINE || "false"),
 	BGMBOT : toBool(process.env.BGMBOT || "true"),
 	API: "https://api-aswin-sparky.koyeb.app",
-	AUDIO_DATA: process.env.AUDIO_DATA || "X BOT MD;ASWIN SPARKY;https://files.catbox.moe/ttdne9.jpg",
+	AUDIO_DATA: process.env.AUDIO_DATA || "ROYAL-XMD;𝐑͢ᴏʏ݉ᴀʟ͎ 𝐒͢ᴇ݉ʀ͎;https://files.catbox.moe/l354bh.jpg",
 	AUTO_STATUS_VIEW: toBool(process.env.AUTO_STATUS_VIEW || "true"),
-	BOT_INFO: process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://url.sparky.biz.id/5ftLiA.jpg",
+	BOT_INFO: process.env.BOT_INFO || "X-BOT;𝐑͢ᴏʏ݉ᴀʟ͎ 𝐒͢ᴇ݉ʀ͎;https://files.catbox.moe/iiuygs.jpg",
 	CALL_BLOCK: toBool(process.env.CALL_BLOCK || "false"),
 	CALL_BLOCK_MSG: process.env.CALL_BLOCK_MSG || "_Calls are not allowed. Please don’t call again!._",
 	DATABASE_URL,
@@ -58,22 +58,22 @@ module.exports = {
 	HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
 	KOYEB_API_KEY: process.env.KOYEB_API_KEY || "",
 	KOYEB_SERVICE_NAME: process.env.KOYEB_SERVICE_NAME || "",
-	RENDER_API_KEY: process.env.RENDER_API_KEY || "",
+	RENDER_API_KEY: process.env.RENDER_API_KEY || "rnd_vW3KOgosY9x4LMgxPpg1OCtGVua6",
 	RENDER_APP_NAME: process.env.RENDER_APP_NAME || "",
 	LANGUAGE: process.env.LANGUAGE || "english",
 	LOGS: toBool(process.env.LOGS || "false"),
 	MENU_TYPE: process.env.MENU_TYPE || "image", // Menu style: big, small, image, document, text, call, payment
 	MENU_FONT: process.env.MENU_FONT || "tiny", // randomStyle, strikeThrough, wingdings, vaporwave, typewriter, analucia, tildeStrikeThrough, underline, doubleUnderline, slashThrough, sparrow, heartsBetween, arrowBelow, crossAboveBelow, creepify, bubbles, mirror, squares, roundsquares, flip, tiny, createMap, serif_I, manga, ladybug, runes, serif_B, serif_BI, serif_I, fancy1, fancy2, fancy3, fancy4, fancy5, fancy6, fancy7, fancy8, fancy9, fancy10, fancy11, fancy12, fancy13, fancy14, fancy15, fancy16, fancy17, fancy18, fancy19, fancy20, fancy21, fancy22, fancy23, fancy24, fancy25, fancy26, fancy27, fancy28, fancy29, fancy30, fancy31, fancy32, fancy33
 	PORT: process.env.PORT || 8080,
-	PING: process.env.PING || "Latency",
+	PING: process.env.PING || "PONG",
 	PM_BLOCK: toBool(process.env.PM_BLOCK || "false"),
 	READ_MESSAGES: toBool(process.env.READ_MESSAGES || "false"),
 	REJECT_CALL: toBool(process.env.REJECT_CALL || "false"),
 	REJECT_CALL_MSG: process.env.REJECT_CALL_MSG || "_Calls are not allowed. Please don’t call again!._",
 	SESSION_ID: process.env.SESSION_ID || "",
 	START_MSG: toBool(process.env.START_MSG || "true"),
-	STICKER_DATA: process.env.STICKER_DATA || `X BOT MD;ASWIN SPARKY`,
-	SUDO: process.env.SUDO || "917012984396",
+	STICKER_DATA: process.env.STICKER_DATA || `X-BOT",𝐑͢ᴏʏ݉ᴀʟ͎ 𝐒͢ᴇ݉ʀ͎`,
+	SUDO: process.env.SUDO || "918075298454",
 	WORK_TYPE: process.env.WORK_TYPE || "public",
 	SAVE_STATUS: toBool(process.env.SAVE_STATUS || "false"),
 	STATUS_REPLY: toBool(process.env.STATUS_REPLY || "false"),
